8dc8912bed5c1b1193cbde4400ccb94d83ab9917
Diego Martinez <dmartinezvergara09@gmail.com>
2017-11-09 20:33:14 -0300

    Modificaciones index

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,14 +6,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
    def facebook
 # Attempt to find the User
-@user=User.find_for_facebook_oauth(
-      request.env["omniauth.auth"]   
-       )
+@user = User.find_for_facebook_oauth(
+  request.env["omniauth.auth"]   
+  )
 if @user.persisted?
-      flash[:notice] ="Has ingresado via Facebook"
-      sign_in_and_redirect @user, :event=>:authentication
+  flash[:notice] ="Has ingresado via Facebook"
+  sign_in_and_redirect @user, :event => :authentication
 else
-      redirect_to new_user_registration_url
+  redirect_to new_user_registration_url
 end
 end
 
